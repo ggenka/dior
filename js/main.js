227c3ca7fad247c6d20dec19c3c7e6484af3a54d
git-test <gggeeennaaa@gmail.com>
2025-02-24 19:09:18 +0200

    added accordeon

--- js/main.js
+++ js/main.js
@@ -22,3 +22,11 @@ const swiperAbaut = new Swiper(".abaut__slider", {
     prevEl: '.swiper-button-prev',
   },
 });
+
+
+document.querySelectorAll('.accordeon__triger').forEach((item) => {
+item.addEventListener('click', () => {
+  item.parentNode.classList.toggle('accordeon__item--active')
+})
+})
+
